8d6f7ebde5b4387b91907d9bf4905bc6bda8de50
denisdelaki <denisdelaki@gmail.com>
2022-06-25 09:27:47 +0300

    completed loggin and registration

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -10,9 +10,14 @@ const profileImage = document.getElementById('image')
 const profileReviews = document.getElementById('review-contact')
 const addReview=document.getElementById('review-button')
 //section 4
-const register=document.getElementById('register')
+const userRegForm = document.getElementById('myForm')
+const registerDiv = document.getElementById("register")
+const loginPage=document.getElementById('login-card')
+const login=document.getElementById('login-form')
 const loginEmail = document.getElementById('email')
 const loginPassword = document.getElementById('password')
+const registerButton = document.getElementById('registerButton')
+const contactForm=document.getElementById('contact-form')
 const contactName = document.getElementById('contactName')
 const contactEmail = document.getElementById('contactEmail')
 const message = document.getElementById('contactTextArea')
@@ -51,8 +56,8 @@ function renderMechanicData(mechanic) {
     reviewInput.placeholder="Add Reviews here"
     let addButton = document.createElement('button')
     addButton.id = "review-button";
-    addButton.onclick='handleClick()'
-    addButton.textContent='Add'
+  addButton.textContent = 'Add'
+   
     //append the children nodes to the parent nodes
     imageDiv.appendChild(profileImage);
     newDiv.appendChild(imageDiv);
@@ -68,25 +73,67 @@ function renderMechanicData(mechanic) {
 }
  //fetch the data from the server
 function getMechanicData() {
-    fetch(" http://localhost:3000/mechanics")
+    fetch("http://localhost:3000/mechanics")
       .then((res) => res.json())
       .then((mechanics) =>
         mechanics.forEach((mechanic) => {
           renderMechanicData(mechanic);
+         
         })
       );
 }
-function registerUsers() {
-  fetch('http://localhost:3000/users')
-    .then(res => res.json())
-    .then(users => {
-      renderPageRegistration(users)
-    })
-}
+
+
 //Event listeners
 profileForm.addEventListener('submit', handleSubmit);
-
+userRegForm.addEventListener('submit', userRegistration);
+document.getElementById('registerLink').addEventListener('click', () => {
+  registerDiv.style.display = 'none'
+  loginPage.style.display="block"
+})
+login.addEventListener('submit', userLogin)
+contactForm.addEventListener('submit', handleMessages)
 //event handlers
+function userLogin(e) {
+  e.preventDefault()
+  let loggedinUser = {
+    email: e.target.email.value,
+    password: e.target.password.value,
+  };
+  if (
+    loggedinUser.email === "denisdelaki@gmail.com" &&
+    loggedinUser.password=== "denis"
+  ) {
+    loginPage.style.display = 'none'
+    registerDiv.style.display = 'none'
+    window.location.href='./index.html'
+    alert("Welcome Back!! You have succesfully logged in")
+  } else {
+    loginPage.style.display = "none";
+    registerDiv.style.display = "block";
+    alert('kindly sign up')
+  }
+}
+
+function userRegistration(e) {
+  e.preventDefault()
+  let user = {
+    name: e.target.name.value,
+    email: e.target.name.value,
+    password1: e.target.password.value,
+    password2: e.target.password.value
+  }
+  registerUser(user)
+} 
+function handleMessages(e) {
+  e.preventDefault();
+  let feedback = {
+  Name: e.target.name.value,
+  Email:e.target.email.value,
+  Message: e.target.contact.value
+  }
+  getMessages(feedback)
+}
 function handleSubmit(e) {
     e.preventDefault()
     let myMechanic = {
@@ -95,51 +142,48 @@ function handleSubmit(e) {
         contacts: e.target.contacts.value,
         image: e.target.image.value
     }
-    renderMechanicData(myMechanic)
+    renderMechanicData(myMechanic);
     createProfile(myMechanic)
 }
-function renderPageRegistration() {
-  let registerForm = document.createElement('form')
-  registerForm.id = 'myForm'
-  let formHeader = document.createElement('h1')
-  formHeader.textContent = "Kindly register Here"
-  let nameInput = document.createElement('input')
-  nameInput.placeholder='Enter your User Name'
-  let emailInput = document.createElement("input");
-  emailInput.textContent = `email: `
-  emailInput.type = 'email'
-  emailInput.placeholder='Enter your Email address'
-  let passwordInput = document.createElement("input");
-  passwordInput.type = 'password'
-  passwordInput.placeholder='Enter your password'
-  let registerButton = document.createElement('button')
-  registerButton.type = 'submit'
-  registerButton.id='registerButton'
-  registerButton.innerText = 'REGISTER NOW'
-  let loginparagra = document.createElement('p')
-  loginparagra.className='loginpara'
-  loginparagra.innerText = ` Already have an account? `
-  let link = document.createElement('a')
-  link.href = "#login-card";
-  link.innerText = "click here to login.";
-
-  //append child nodes to the parent nodes 
-  document.getElementById('login-registration').appendChild(register)
-  register.appendChild(registerForm)
-  registerForm.appendChild(formHeader)
-  registerForm.appendChild(nameInput);
-  registerForm.appendChild(emailInput);
-  registerForm.appendChild(passwordInput);
-  registerForm.appendChild(registerButton)
-  loginparagra.appendChild(link)
-  registerForm.appendChild(loginparagra)
-
-
+function registerUser(user) {
+  fetch("http://localhost:3000/RegisteredUsers", {
+    method: "POST",
+    headers: {
+      "content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  }).then(res => res.json())
+  .then(user=>console.log(user))
+   
 }
+// document.querySelector('#myForm').addEventListener('submit', handleUserRegistration);
+// function handleUserRegistration(e) {
+  
+//    let user = {
+//      name: e.target.document.getElementById("formName").value,
+//      email: e.target.document.getElementById("formEmail").value,
+//      password: e.target.document.getElementById("formPassword").value,
+//    };
+ 
+//   registerUsers(user);
+ 
+// }
 
-function handleClick() {
- console.log('hello')
-}
+// function registerUsers(user) {
+//   e.preventDefault();
+//   fetch("http://localhost:3000/users", {
+//     method: "POST",
+//     headers: {
+//       "content-Type": "application/json",
+//     },
+//     body: JSON.stringify(user),
+//   })
+
+//     .then((res) => res.json())
+//     .then(user => { console.log(user)
+// })
+//   // .catch(error=>console.error(error))
+// }
 //post the data when the user create a profile
 function createProfile(myMechanic) {
   fetch(" http://localhost:3000/mechanics", {
@@ -154,9 +198,20 @@ function createProfile(myMechanic) {
   
 }
 
+function getMessages(feedback) {
+  fetch("http://localhost:3000/messages", {
+    method: 'POST',
+    headers: {
+      'content-Type': 'application/json'
+    },
+    body: JSON.stringify(feedback)
+  })
+    .then(res => res.json())
+  .then(feedback=>console.log(feedback))
+}
  //display the data on  on the DOM
  function displayMechanicData() {
    getMechanicData()
-   renderPageRegistration();
+  //  renderPageRegistration();
  }
 displayMechanicData()
